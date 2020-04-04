7c3ab7fc940bcdfcc48fc6689a53267ac2b5abeb
Mahmoud <mahwafa2005@gmail.com>
2020-04-04 14:12:46 +0300

    first commit in jsf-docker example

---
+++ jsf-docker/src/main/java/com/jsf/docker/HelloBean.java
@@ -0,0 +1,18 @@
+package com.jsf.docker;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable {
+  private String name;
+  public String getName(){
+    return name;
+  }
+  
+  public void setName(String name){
+    this.name = name;
+  }
+}
